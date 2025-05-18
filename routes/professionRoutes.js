4f67bc5a25105986f6c0654d79f8bda0db7c296f
Krishna25102002 <tripdaries01@gmail.com>
2025-05-18 15:17:57 +0530

    Update professionRoutes.js

--- routes/professionRoutes.js
+++ routes/professionRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { addProfession, getProfessionalsByService } = require("../controllers/professionController");
+const { addProfession, getProfessionalsByService, updateUserProfessionalProfile } = require("../controllers/professionController");
 const { protect } = require('../middlewares/authMiddleware'); // Import the protect middleware
 
 router.post('/', protect, addProfession); // Apply protect middleware here
 router.get('/', getProfessionalsByService);
+router.put('/me', protect, updateUserProfessionalProfile); // if base path is /api/professions
+
 
 
 
